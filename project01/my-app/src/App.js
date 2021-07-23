01191fba085bf87c5378082cdb8422ed73ce5be8
OVOlha <oslavuta@gmail.com>
2021-07-23 14:18:53 +0300

    m

---
+++ project01/my-app/src/App.js
@@ -0,0 +1,46 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+
+
+
+
+let App = () => {
+  return (
+
+    <div className='app-wrapper'>
+      <header className='header'>
+        <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/LEGO_logo.svg/512px-LEGO_logo.svg.png' alt="site-logo">
+        </img>
+      </header>
+
+      <nav className='nav'>
+        <div>
+          <a>Profile</a>
+        </div>
+        <div>
+          <a href src='#'>Messages</a>
+        </div>
+        <div>
+          <a href src='#$'>News</a>
+        </div>
+        <div>
+          <a href src='#$'>Music</a>
+        </div>
+        <div>
+          <a href src='#$'>Settings</a>
+        </div>
+      </nav>
+      <div className='content'>
+
+      </div>
+
+    </div>
+
+  );
+}
+
+
+
+export default App;
